ed52f6294ee62bf7bbeacad7e1c12c9f1cfc8e17
Adam Martinez <adam@potablepoison.com>
2013-09-06 16:28:58 -0700

    mostly done

---
+++ src/chapter/two/hello_world/AttackActivity.java
@@ -0,0 +1,14 @@
+package chapter.two.hello_world;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AttackActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_attack);
+	}
+
+}
---
+++ src/chapter/two/hello_world/TravelActivity.java
@@ -0,0 +1,14 @@
+package chapter.two.hello_world;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TravelActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_travel);
+	}
+
+}
